7528ebd4689b8bb65bddf210245efdb0eba92542
Ragul Balu <ragulbalu19@gmail.com>
2026-02-17 23:14:45 +0530

    Added health check route

--- freelancemarket/urls.py
+++ freelancemarket/urls.py
@@ -33,10 +33,14 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+from django.http import HttpResponse
+
+def healthz(request):
+    return HttpResponse("OK")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path("healthz", healthz),
     path('', include('gigs.urls')),
     path('accounts/', include('accounts.urls')),
     path('orders/', include('orders.urls')),
